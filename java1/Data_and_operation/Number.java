33e473bc9ea9cf3c06dc636799900ea113e1d323
kek980 <52143311+kek980@users.noreply.github.com>
2020-12-08 18:57:07 +0900

    Add files via upload

---
+++ java1/Data_and_operation/Number.java
@@ -0,0 +1,21 @@
+
+/**
+ * @author admin
+ *
+ */
+public class Number {
+
+	public static void main(String[] args) {
+		
+		System.out.println(6+2);
+		System.out.println(6-2);
+		System.out.println(6*2);
+		System.out.println(6/2);
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.ceil(Math.PI));
+		
+
+	}
+
+}
